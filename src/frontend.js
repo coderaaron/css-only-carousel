c1960b09290e46a7e1ba0051b936bf3d8562dc12
Aaron Graham <aegraham@gmail.com>
2022-09-22 11:12:54 -0500

    Add comment about inline JS

--- src/frontend.js
+++ src/frontend.js
@@ -1,3 +1,4 @@
 window.onload = () => {
+	// This code has been moved to inline JS to accomodate multiple carousels on the same page
 	//parseInt( document.querySelector( 'input[name=carousel]:checked' ).value );
 };
